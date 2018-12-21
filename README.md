af2af4deaf60074347022e48afa6214698de73da
Pankaj Kumar Singh <13923440+03Pankaj14@users.noreply.github.com>
2018-12-21 19:43:54 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-programming
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter.jsx";
+
+//const element = <h1>Hello world</h1>;
+//console.log(element);
+ReactDOM.render(<Counter />, document.getElementById("root"));
